8f057e4527a82ceb977a5f549e4dfacca9f76cbf
rangel.andrade <rangel.andrade@ABR-OF-WL0222.na.autoliv.int>
2020-09-23 23:03:36 -0300

    commit inicial exercicio LP1

---
+++ LP-1/src/exercicio/Profissional.java
@@ -0,0 +1,33 @@
+package exercicio;
+//Classe
+public class Profissional {
+
+    //Atributos
+    String nome;
+    String departamento;
+    int ID;
+    float salario;
+    String cargo;
+    
+    //Construtor
+    public Profissional(String c, String nome){
+        cargo = c;
+        this.nome = nome;
+    }
+    
+    //Métodos de acesso do tipo getters e setters
+    public void setDepartamento(String departamento){
+        this.departamento = departamento;
+    }
+    
+    public String getDepartamento(){
+        return departamento;
+    }
+    
+     public static void main(String []args){
+         // Objeto
+         Profissional P=new Profissional("Programador", "Rangel");
+         P.setDepartamento("Engenharia");
+        System.out.println (P.getDepartamento());
+    }
+}
